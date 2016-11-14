de85b3ee713e7c8911efeab3c2b32bcc53a6cfa7
daler <dalerr@niddk.nih.gov>
2016-11-14 11:21:58 -0500

    add cutadapt test

---
+++ test/test_cutadapt.py
@@ -0,0 +1,30 @@
+import os
+import pytest
+import gzip
+import tempfile
+import shutil
+from utils import run, dpath, rm, symlink_in_tempdir
+
+def test_cutadapt_simple(sample1_se_fq):
+    snakefile = '''
+                rule cutadapt:
+                    input:
+                        fastq='sample1_R1.fastq.gz'
+                    output:
+                        fastq='sample1_R1.trim.fastq.gz'
+                    params: extra='-a AAA'
+                    wrapper: {wrapper}
+                '''
+    input_data_func=symlink_in_tempdir(sample1_se_fq, 'sample1_R1.fastq.gz')
+
+    def check():
+        """
+        check for line lengths and that they are at least different sized
+        """
+        a = sum(1 for _ in gzip.open('sample1_R1.fastq.gz'))
+        b = sum(1 for _ in gzip.open('sample1_R1.trim.fastq.gz'))
+        assert a == b == 9924
+
+        assert os.path.getsize('sample1_R1.fastq.gz') != os.path.getsize('sample1_R1.trim.fastq.gz')
+
+    run(dpath('../wrappers/cutadapt'), snakefile, check, input_data_func)
