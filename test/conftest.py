c3dc10d1f0468d07900cc1a2fae2169f86eb3f3e
daler <dalerr@niddk.nih.gov>
2016-11-15 18:07:38 -0500

    let pytest handle tmpdir cleanup
    that way, tempfiles will stick around for a little while after tests so
    we can later inspect

--- test/conftest.py
+++ test/conftest.py
@@ -11,8 +11,7 @@ def sample1_se_fq(tmpdir_factory):
     url = 'https://github.com/lcdb/lcdb-test-data/blob/add-data/data/{}?raw=true'.format(fn)
     basename = os.path.basename(fn)
     shell('wget -O- {url} > {d}/{basename}')
-    yield os.path.join(d, basename)
-    shutil.rmtree(d)
+    return os.path.join(d, basename)
 
 
 @pytest.fixture(scope='session')
