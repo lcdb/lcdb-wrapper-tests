ef713dda63351f946641193c8044c00fe9dea435
daler <dalerr@niddk.nih.gov>
2016-11-16 13:58:05 -0500

    add hisat2-align wrapper plus tests

--- test/test_hisat2.py
+++ test/test_hisat2.py
@@ -1,5 +1,9 @@
+import pytest
+import pysam
 from snakemake.shell import shell
+from lcdblib.snakemake import aligners
 from utils import run, dpath, rm, symlink_in_tempdir
+
 
 
 def test_hisat2_build(dm6_fa, tmpdir):
@@ -24,3 +28,67 @@ def test_hisat2_build(dm6_fa, tmpdir):
         assert list(shell('hisat2-inspect data/assembly/assembly -n', iterable=True)) == ['2L']
 
     run(dpath('../wrappers/hisat2/build'), snakefile, check, input_data_func, tmpdir)
+
+
+def _dict_of_hisat2_indexes(hisat2_indexes, prefix):
+    d = {}
+    indexes = aligners.hisat2_index_from_prefix('seq/2L')
+    hisat2_indexes = sorted(hisat2_indexes)
+    indexes = sorted(indexes)
+    for k, v in zip(hisat2_indexes, indexes):
+        d[k] = v
+    return d
+
+
+def test_hisat2_align_se(hisat2_indexes, sample1_se_fq, tmpdir):
+    d = _dict_of_hisat2_indexes(hisat2_indexes, '2L')
+    indexes = list(d.values())
+    snakefile = '''
+        rule hisat2_align:
+            input:
+                fastq='sample1_R1.fastq.gz',
+                index={indexes}
+            output:
+                bam='sample1.bam'
+            log: "hisat2.log"
+            wrapper: "file://wrapper"
+    '''.format(indexes=indexes)
+    d[sample1_se_fq] = 'sample1_R1.fastq.gz'
+    input_data_func = symlink_in_tempdir(d)
+
+    def check():
+        assert "overall alignment rate" in open('hisat2.log').read()
+
+        # should have at least some mapped and unmapped
+        assert int(list(shell('samtools view -c -f 0x04 sample1.bam', iterable=True))[0]) > 0
+        assert int(list(shell('samtools view -c -F 0x04 sample1.bam', iterable=True))[0]) > 0
+
+    run(dpath('../wrappers/hisat2/align'), snakefile, check, input_data_func, tmpdir)
+
+
+def test_hisat2_align_se_rm_unmapped(hisat2_indexes, sample1_se_fq, tmpdir):
+    d = _dict_of_hisat2_indexes(hisat2_indexes, '2L')
+    indexes = list(d.values())
+    snakefile = '''
+        rule hisat2_align:
+            input:
+                fastq='sample1_R1.fastq.gz',
+                index={indexes}
+            output:
+                bam='sample1.bam'
+            params:
+                samtools_view_extra='-F 0x04'
+            log: "hisat2.log"
+            wrapper: "file://wrapper"
+    '''.format(indexes=indexes)
+    d[sample1_se_fq] = 'sample1_R1.fastq.gz'
+    input_data_func = symlink_in_tempdir(d)
+
+    def check():
+        assert "overall alignment rate" in open('hisat2.log').read()
+
+        # should have at least some mapped and unmapped
+        assert int(list(shell('samtools view -c -f 0x04 sample1.bam', iterable=True))[0]) == 0
+        assert int(list(shell('samtools view -c -F 0x04 sample1.bam', iterable=True))[0]) > 0
+
+    run(dpath('../wrappers/hisat2/align'), snakefile, check, input_data_func, tmpdir)
