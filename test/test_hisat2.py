4746603b37f48ab464b88dbe53b26861e7555710
Justin Fear <justin.m.fear@gmail.com>
2016-12-05 14:26:37 -0500

    Adds basic hisat2 SRA test.
    I found an SRA that only has 1000 reads, uses this SRA to test hist2
    alignment.

--- test/test_hisat2.py
+++ test/test_hisat2.py
@@ -65,6 +65,31 @@ def test_hisat2_align_se(hisat2_indexes, sample1_se_fq, tmpdir):
     run(dpath('../wrappers/hisat2/align'), snakefile, check, input_data_func, tmpdir)
 
 
+def test_hisat2_align_se_SRA(hisat2_indexes, tmpdir):
+    d = _dict_of_hisat2_indexes(hisat2_indexes, '2L')
+    indexes = list(d.values())
+    snakefile = '''
+        rule hisat2_align:
+            input:
+                index={indexes}
+            output:
+                bam='sample1.bam'
+            params: hisat2_extra='--sra-acc SRR1990338'
+            log: "hisat2.log"
+            wrapper: "file://wrapper"
+    '''.format(indexes=indexes)
+    input_data_func = symlink_in_tempdir(d)
+
+    def check():
+        assert "overall alignment rate" in open('hisat2.log').read()
+
+        # should have at least some mapped and unmapped
+        assert int(list(shell('samtools view -c -f 0x04 sample1.bam', iterable=True))[0]) > 0
+        assert int(list(shell('samtools view -c -F 0x04 sample1.bam', iterable=True))[0]) > 0
+
+    run(dpath('../wrappers/hisat2/align'), snakefile, check, input_data_func, tmpdir)
+
+
 def test_hisat2_align_se_rm_unmapped(hisat2_indexes, sample1_se_fq, tmpdir):
     d = _dict_of_hisat2_indexes(hisat2_indexes, '2L')
     indexes = list(d.values())
