189769f1d997d54a13db6e413dd4f4cf55727f4e
Justin Fear <justin.m.fear@gmail.com>
2016-12-07 15:52:48 -0500

    Working infer_experiment.

--- test/test_rseqc.py
+++ test/test_rseqc.py
@@ -1,6 +1,7 @@
 import os
 import gzip
 from utils import run, dpath, rm, symlink_in_tempdir
+from textwrap import dedent
 
 def test_infer_experiment(sample1_se_bam, annotation_bed12, tmpdir):
     snakefile = '''
@@ -22,8 +23,15 @@ def test_infer_experiment(sample1_se_bam, annotation_bed12, tmpdir):
         """
         check for line lengths and that they are at least different sized
         """
+        expected = dedent("""\
+                This is SingleEnd Data
+                Fraction of reads failed to determine: 0.1166
+                Fraction of reads explained by "++,--": 0.8820
+                Fraction of reads explained by "+-,-+": 0.0014""")
+
         with open('sample1_R1.infer_experiment.txt', 'r') as handle:
-            line = handle.readline()
-            assert line == 'This is SingleEnd Data'
+            results = handle.read().strip()
+
+        assert  results == expected
 
     run(dpath('../wrappers/rseqc/infer_experiment'), snakefile, check, input_data_func, tmpdir, use_conda=True)
