eacc446c40207fe25f2b56319db9a72d8217faa3
Justin Fear <justin.m.fear@gmail.com>
2016-12-29 14:03:09 -0500

    Cleans up wrapper.

--- wrappers/preseq/observed_complexity/wrapper.py
+++ wrappers/preseq/observed_complexity/wrapper.py
@@ -4,9 +4,4 @@ from snakemake.shell import shell
 log = snakemake.log_fmt_shell()
 extra = snakemake.params.get('extra', '')
 
-try:
-    extra = snakemake.params.extra
-except AttributeError:
-    extra = ""
-
 shell("preseq c_curve {extra} -B {snakemake.input.bam} -o {snakemake.output.table}")
