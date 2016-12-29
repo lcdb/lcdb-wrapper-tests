1e3eacfc3b722cf926140dee81a684ed8adcd7f0
Ryan Dale <dalerr@niddk.nih.gov>
2016-12-29 15:38:59 -0500

    add lcdb channel and lcdblib

--- travis-setup.sh
+++ travis-setup.sh
@@ -12,6 +12,7 @@ conda config --add channels conda-forge
 conda config --add channels defaults
 conda config --add channels r
 conda config --add channels bioconda
+conda config --add channels lcdb
 
 conda install -y --file requirements.txt
 pip install -r pip-requirements.txt
