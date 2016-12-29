3fd2f5022c62e0645533ef5863da406b9e868950
Ryan Dale <dalerr@niddk.nih.gov>
2016-12-29 11:04:27 -0500

    remove vestigial osx support; disable sudo for faster builds

--- travis-setup.sh
+++ travis-setup.sh
@@ -3,15 +3,8 @@ set -euo pipefail
 set -x
 
 # Sets up travis-ci environment for testing bioconda-utils.
-
-if [[ $TRAVIS_OS_NAME = "linux" ]]
-then
-    tag=Linux
-else
-    tag=MacOSX
-fi
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-${tag}-x86_64.sh
-sudo bash Miniconda3-latest-${tag}-x86_64.sh -b -p /anaconda
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
 sudo chown -R $USER /anaconda
 export PATH=/anaconda/bin:$PATH
 
