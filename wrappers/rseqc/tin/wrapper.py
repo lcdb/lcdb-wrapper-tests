b39959b535ef4f11cff40ead55824ab2559ded3f
Justin Fear <justin.m.fear@gmail.com>
2017-02-10 16:43:06 -0500

    Changes rseqc to heavily use TMPDIR.

--- wrappers/rseqc/tin/wrapper.py
+++ wrappers/rseqc/tin/wrapper.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
 
 import os
+from tempfile import NamedTemporaryFile
 from snakemake.shell import shell
 
 # All wrappers must be able to handle an optional params.extra.
 extra = snakemake.params.get('extra', '')
 
-
 # This lets us handle whether to write to a log file or to write to stdout.
 # See snakemake.script.log_fmt_shell for details.
 log = snakemake.log_fmt_shell()
 
+# tin uses the name of the BAM to create outputs. In order to write outputs to
+# tmp I need to copy the BAM over to tmp.
+bam = NamedTemporaryFile(suffix='.bam').name
+bed = NamedTemporaryFile(suffix='.bed').name
+
+shell(
+    'cp {snakemake.input.bam} {bam} '
+    '&& cp {snakemake.input.bam}.bai {bam}.bai '
+    '&& cp {snakemake.input.bed} {bed}')
+
 shell(
     'tin.py '
-    '-i {snakemake.input.bam} '
-    '-r {snakemake.input.bed} '
+    '-i {bam} '
+    '-r {bed} '
     '{extra} '
     '{log}')
 
-name = os.path.basename(snakemake.input.bam).rstrip('.bam')
+name = os.path.basename(bam).rstrip('.bam')
 
 shell(
         'mv {name}.tin.xls {snakemake.output.table} '
