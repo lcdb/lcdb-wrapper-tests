9a796e5d9d5a2e96ca6e7d2ee175dcddc8986bb4
Justin Fear <justin.m.fear@gmail.com>
2017-02-14 16:57:46 -0500

    hotfix tin

--- wrappers/rseqc/tin/wrapper.py
+++ wrappers/rseqc/tin/wrapper.py
@@ -8,7 +8,9 @@ from snakemake.shell import shell
 extra = snakemake.params.get('extra', '')
 # This lets us handle whether to write to a log file or to write to stdout.
 # See snakemake.script.log_fmt_shell for details.
-snakemake.log = os.path.realpath(str(snakemake.log))
+if snakemake.log:
+    snakemake.log = os.path.realpath(str(snakemake.log))
+
 log = snakemake.log_fmt_shell(stdout=False)
 
 # Get directories that I need to move between
