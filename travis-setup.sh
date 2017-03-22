11022faa117994965b7b41062e6ac6d4fcbc0e4e
Justin Fear <justin.m.fear@gmail.com>
2017-03-22 15:55:11 -0400

    adds -n root so env update works.

--- travis-setup.sh
+++ travis-setup.sh
@@ -15,4 +15,4 @@ conda config --add channels bioconda
 conda config --add channels lcdb
 
 conda install -y python=3.5
-conda env update --file environment.yaml
+conda env update -n root --file environment.yaml
